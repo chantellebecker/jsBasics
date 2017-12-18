84e283b360e129c869b7190eca49cc38b2685c1f
Chantelle Becker <cbecker@pendo.io>
2017-12-18 11:58:25 -0500

    add to madlib file

--- madlib.js
+++ madlib.js
@@ -1,8 +1,14 @@
 "<h2>One day a [adjective] child wanted to [verb] to see their [noun], but was unable to because of work.</h2>"
 
-var adjective = prompt("Please type an adjective");
-var verb = prompt("Please type a verb");
-var noun = prompt("Please type a noun");
+var questions = 3;
+var questionsLeft = ' [' + questions + ' questions left]';
+var adjective = prompt("Please type an adjective" + questionsLeft);
+questions -= 1;
+questionsLeft = ' [' + questions + ' questions left]';
+var verb = prompt("Please type a verb" + questionsLeft);
+questions -= 1;
+questionsLeft = ' [' + questions + ' questions left]';
+var noun = prompt("Please type a noun" + questionsLeft);
 alert("All done! Ready to see your madlib?");
 
 var sentence = "<h2>One day a " + adjective;
