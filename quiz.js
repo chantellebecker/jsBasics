f249985cae4d4c4fe072e8bc87ac05437511ae10
Chantelle Becker <cbecker@pendo.io>
2018-01-09 12:28:07 -0500

    toUpperCase

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 var answer = prompt('What programming language is the name of a gem?');
-if ( answer === 'Ruby') {
+if ( answer.toUpperCase() === 'RUBY') {
   document.write("<p>That's right!</p>");
 } else {
   document.write("<p>Sorry, that's wrong.</p>")
